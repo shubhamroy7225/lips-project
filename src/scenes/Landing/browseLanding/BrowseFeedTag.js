c2901c910e57a41d08f0952ddd1c0c8a2764b790
diksha shivhare <diksha@bitcot.com>
2020-08-26 23:47:06 +0530

    set state in selected tags in provide browse tags page

--- src/scenes/Landing/browseLanding/BrowseFeedTag.js
+++ src/scenes/Landing/browseLanding/BrowseFeedTag.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom"
 export default () => {
+  const [selectTags, setSelectTags] = useState([])
   return (
     <div className="limiter">
        <div className="container-login100">
@@ -16,24 +17,24 @@ export default () => {
                       </article>
                       <ul className="lps_btn_grps lps_ul lps_hash_ul">
                          <li>
-                            <button className="theme_btn theme_outline_light">#Hashtag</button>
-                            <button className="theme_btn theme_outline_light">#Hashtag</button>
-                            <button className="theme_btn theme_outline_light">#Hashtag</button>
+                            <button className="theme_btn theme_outline_light" onClick={() => setSelectTags([...selectTags, "selected"])}>#Hashtag</button>
+                            <button className="theme_btn theme_outline_light" onClick={() => setSelectTags([...selectTags, "selected"])}>#Hashtag</button>
+                            <button className="theme_btn theme_outline_light" onClick={() => setSelectTags([...selectTags, "selected"])}>#Hashtag</button>
                          </li>
                          <li>
-                            <button className="theme_btn theme_outline_light">#Hashtag</button>
-                            <button className="theme_btn theme_outline_light">#Hashtag</button>
-                            <button className="theme_btn theme_outline_light">#Hashtag</button>
+                            <button className="theme_btn theme_outline_light" onClick={() => setSelectTags([...selectTags, "selected"])}>#Hashtag</button>
+                            <button className="theme_btn theme_outline_light" onClick={() => setSelectTags([...selectTags, "selected"])}>#Hashtag</button>
+                            <button className="theme_btn theme_outline_light" onClick={() => setSelectTags([...selectTags, "selected"])}>#Hashtag</button>
                          </li>
                          <li className="lps_pos_rltv">
-                            <button className="theme_btn theme_outline_light">#Hashtag</button>
-                            <button className="theme_btn theme_outline_light">#Hashtag</button>
-                            <button className="theme_btn theme_outline_light">#Hashtag</button>
+                            <button className="theme_btn theme_outline_light" onClick={() => setSelectTags([...selectTags, "selected"])}>#Hashtag</button>
+                            <button className="theme_btn theme_outline_light" onClick={() => setSelectTags([...selectTags, "selected"])}>#Hashtag</button>
+                            <button className="theme_btn theme_outline_light" onClick={() => setSelectTags([...selectTags, "selected"])}>#Hashtag</button>
                          </li>
                          <li>
-                            <button className="theme_btn theme_outline_light">#Hashtag</button>
-                            <button className="theme_btn theme_outline_light">#Hashtag</button>
-                            <button className="theme_btn theme_outline_light">#Hashtag</button>
+                            <button className="theme_btn theme_outline_light" onClick={() => setSelectTags([...selectTags, "selected"])}>#Hashtag</button>
+                            <button className="theme_btn theme_outline_light" onClick={() => setSelectTags([...selectTags, "selected"])}>#Hashtag</button>
+                            <button className="theme_btn theme_outline_light" onClick={() => setSelectTags([...selectTags, "selected"])}>#Hashtag</button>
                          </li>
                       </ul>
                       <div className="pos_wrp onboarding_btm">
