b62dc2fb3dacb3530faf372a7815944dee671949
pappudas bairagi <pappudas@bitcot.com>
2020-08-27 17:00:57 +0530

    add flow to register

--- src/scenes/Auth/Register.js
+++ src/scenes/Auth/Register.js
@@ -1,66 +1,73 @@
 import React from "react";
+import {useHistory} from "react-router-dom";
 
 export default () => {
+  const history = useHistory();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history.push("/terms-and-condition");
+  };
+
   return (
-      <div class="limiter">
-        <div class="container-login100">
-          <div id="wrap" class="mt_0">
-            <div class="lps_container">
-              <div class="lps_flx_vm_jc lps_bg_txt_white lps_bg_secondary on_boarding_wrp">
-                <div class="lps_form_wrp">
-                  <form>
-                    <article class="text_center lps_logo_center">
-                      <a class="logo mb_0" href="#">
-                        <img src={require("assets/images/thumbnails/logo.png")} alt="Lips Logo" class="header__logo" />
-                          <h5 class="text_uppercase text_white">Register</h5>
-                        </a>
-                      </article>
-                      <div class="lps_fields">
-                        <div class="form_group_modify">
-                          <input type="text" class="input_modify" placeholder="Username" />
-                          </div>
-                          <div class="form_group_modify">
-                            <input type="email" class="input_modify" placeholder="Email" />
-                            </div>
-                            <div class="form_group_modify lps_pos_rltv">
-                              <input type="password" class="input_modify" placeholder="Password" />
-                    <span class="icn_passAbslt">
-                      <img src={require("assets/images/icons/icb_eye_white.png")} />
-                      </span>
-                  </div>
-                                <div class="form_group_modify lps_pos_rltv">
-                                  <input type="password" class="input_modify" placeholder="Repeat Password" />
-                    <span class="icn_passAbslt">
-                      <img src={require("assets/images/icons/icb_eye_white.png")} />
-                      </span>
-                  </div>
-                                    <div class="mt_25">
-                                      <div class="form_group_modify">
-                                        <label class="lps_cont_check">I am 18 years or older
-                                          <input type="checkbox" />
-                                            <span class="lps_Checkmark"></span>
-                                          </label>
-                                        </div>
-                                        <div class="form_group_modify">
-                                          <label class="lps_cont_check">Keep me signed in
-                                            <input type="checkbox" />
-                                              <span class="lps_Checkmark"></span>
-                                            </label>
-                                          </div>
-                                        </div>
-                                      </div>
-                                      <div class="pos_wrp onboarding_btm">
-                                        <a href="terms_conditions.html" class="theme_btn theme_primary btn_block theme_btn_rds25 text_uppercase">Review
-                                          Terms & Conditions</a>
-                                        <p class="btm_links mt_25 text_white">Have an account? <a href="sign_in.html"
-                                                                                                  class="link_underline lps_link">Sign in</a></p>
-                                      </div>
-                                    </form>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
+      <div className="limiter">
+        <div className="container-login100">
+          <div id="wrap" className="mt_0">
+            <div className="lps_container">
+              <div className="lps_flx_vm_jc lps_bg_txt_white lps_bg_secondary on_boarding_wrp">
+                <div className="lps_form_wrp">
+                  <form onSubmit={handleSubmit}>
+                    <article className="text_center lps_logo_center">
+                      <a className="logo mb_0" href="#">
+                        <img src={require("assets/images/thumbnails/logo.png")} alt="Lips Logo" className="header__logo" />
+                        <h5 className="text_uppercase text_white">Register</h5>
+                      </a>
+                    </article>
+                    <div className="lps_fields">
+                      <div className="form_group_modify">
+                        <input type="text" className="input_modify" placeholder="Username" name="username" required/>
+                      </div>
+                      <div className="form_group_modify">
+                        <input type="email" className="input_modify" placeholder="Email" name="email" required/>
+                      </div>
+                      <div className="form_group_modify lps_pos_rltv">
+                        <input type="password" className="input_modify" placeholder="Password" name="password" required/>
+                        <span className="icn_passAbslt">
+                          <img src={require("assets/images/icons/icb_eye_white.png")} />
+                        </span>
+                      </div>
+                      <div className="form_group_modify lps_pos_rltv">
+                        <input type="password" className="input_modify" placeholder="Repeat Password" name="password_confirmation" required/>
+                        <span className="icn_passAbslt">
+                          <img src={require("assets/images/icons/icb_eye_white.png")} />
+                        </span>
+                      </div>
+                      <div className="mt_25">
+                        <div className="form_group_modify">
+                          <label className="lps_cont_check">I am 18 years or older
+                            <input type="checkbox" />
+                            <span className="lps_Checkmark"></span>
+                          </label>
                         </div>
+                        <div className="form_group_modify">
+                          <label className="lps_cont_check">Keep me signed in
+                            <input type="checkbox" />
+                            <span className="lps_Checkmark"></span>
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="pos_wrp onboarding_btm">
+                      <button type="submit" className="theme_btn theme_primary btn_block theme_btn_rds25 text_uppercase">Review
+                        Terms & Conditions</button>
+                      <p className="btm_links mt_25 text_white">Have an account? <a href="sign_in.html"
+                                                                                className="link_underline lps_link">Sign in</a></p>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
   )
 }
