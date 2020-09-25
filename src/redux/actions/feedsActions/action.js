5c6f217872bd2d612a25cd3e1f84c59cbb1664a0
pappudas bairagi <pappudas@bitcot.com>
2020-09-25 17:09:52 +0530

    add loader

--- src/redux/actions/feedsActions/action.js
+++ src/redux/actions/feedsActions/action.js
@@ -4,27 +4,33 @@ import store from '../../../redux/store/store';
 import { hashTagPending, hashTagSuccessful, userhashTagPending, userhashTagSuccessful } from 'redux/actions/feed';
 
 export const getAllHashTags = (credentials) =>  {
+  commonService.isLoading.onNext(true);
   store.dispatch(hashTagPending());
   return API.getAllHashTags(credentials)
       .then(response => {
+        commonService.isLoading.onNext(false);
         store.dispatch(hashTagSuccessful(response.data))
         return response
       })
 }
 
 export const getUserHashTags = (credentials) =>  {
+  commonService.isLoading.onNext(true);
   store.dispatch(userhashTagPending());
   return API.getUserHashTags(credentials)
       .then(response => {
+        commonService.isLoading.onNext(false);
         store.dispatch(userhashTagSuccessful(response.data))
         return response
       })
 }
 
 export const setFavoriteAvoidTags = (credentials) =>  {
+  commonService.isLoading.onNext(true);
   store.dispatch(hashTagPending());
   return API.setFavoriteAvoidTags(credentials)
       .then(response => {
+        commonService.isLoading.onNext(false);
         return response;
       })
 }
