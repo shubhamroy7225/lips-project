477df08ddf94019b0cc1791273a85ce4d88338e7
pappudas bairagi <pappudas@bitcot.com>
2020-09-26 14:53:35 +0530

    setting content changes

--- src/scenes/Setting/FeedSetting/index.js
+++ src/scenes/Setting/FeedSetting/index.js
@@ -26,42 +26,7 @@ const FeedSetting = () => {
               <span className="lp_left_auto text_black">Feed Settings</span>
             </Link>
             <ul className="lps_list_group my_acctn_list my_acctn_list_pl0">
-              <li className="list-group-item">
-                <div className="lps_user_info lps_flx_vm_jsbtwn lps_f_vm lps_mb15">
-                  <article>
-                    <h5 className="ft_Weight_500 mb_5">This product</h5>
-                    <p className="mb_0">is meant for educational purposes only.</p>
-                  </article>
-                  <label className="lps_switch">
-                    <input type="checkbox" checked/>
-                    <span className="lps_int_slider round"></span>
-                  </label>
-                </div>
-              </li>
-              <li className="list-group-item">
-                <div className="lps_user_info lps_flx_vm_jsbtwn lps_f_vm lps_mb15">
-                  <article>
-                    <h5 className="ft_Weight_500 mb_5">This product</h5>
-                    <p className="mb_0">is meant for educational purposes only.</p>
-                  </article>
-                  <label className="lps_switch">
-                    <input type="checkbox" checked/>
-                    <span className="lps_int_slider round"></span>
-                  </label>
-                </div>
-              </li>
-              <li className="list-group-item">
-                <div className="lps_user_info lps_flx_vm_jsbtwn lps_f_vm lps_mb15">
-                  <article>
-                    <h5 className="ft_Weight_500 mb_5">This product</h5>
-                    <p className="mb_0">is meant for educational purposes only.</p>
-                  </article>
-                  <label className="lps_switch">
-                    <input type="checkbox" checked/>
-                    <span className="lps_int_slider round"></span>
-                  </label>
-                </div>
-              </li>
+
               <UserHashtag setEditTag={setEditTag} showhashTags={showhashTags} hideHashtag={hideHashtag}/>
               <BlockUser/>
             </ul>
