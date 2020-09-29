e1ba72397da8c4201f93a44c05ac76dbae1da9a0
pappudas bairagi <pappudas@bitcot.com>
2020-09-29 18:49:13 +0530

    text changes

--- src/scenes/Setting/MyAccount/components/PrivacySetting.js
+++ src/scenes/Setting/MyAccount/components/PrivacySetting.js
@@ -16,13 +16,13 @@ export default ({user}) => {
           <p className="user_info_label lps_mb10">Account privacy</p>
           <label className="lps_cont_rdo">
             <span className="ft_Weight_500">Public</span> <br/>
-            Anyone on the internet can see* your *posts
+            Anyone on the internet can see your posts
             <input type="radio" value={privacy_settings} checked={privacy_settings === "public"} name="radio" onClick={e=> [setPrivacy("public"),  changePrivacyPolicy("public")]} />
             <span className="lps_checkmark"></span>
           </label>
           <label className="lps_cont_rdo">
             <span className="ft_Weight_500">Private</span><br/>
-            Only* people* who follow you can see your posts
+            Only people who follow you can see your posts
             <input type="radio" name="radio" value={privacy_settings} checked={privacy_settings === "private"} onClick={e=> [setPrivacy("private"), changePrivacyPolicy("private")]}  />
             <span className="lps_checkmark"></span>
           </label>
