aba96b1e41aec9c37ed6de48aa1ce74d5a4e9f3c
diksha shivhare <diksha@bitcot.com>
2020-09-29 17:56:44 +0530

    LIPS-21 Settings - My Accounts Deactivate Accounts and Delete Accounts option does not respond

--- src/scenes/Setting/MyAccount/index.js
+++ src/scenes/Setting/MyAccount/index.js
@@ -38,13 +38,6 @@ export default ()  => {
             <PrivacySetting user={user}/>
             <li className="list-group-item">
               <div className="lps_user_info lps_accnt_links">
-                <p className="user_info_label lps_mb15">
-                  <Link to="#" className="ft_Weight_500">Deactivate Account</Link>
-                </p>
-              </div>
-            </li>
-            <li className="list-group-item">
-              <div className="lps_user_info lps_accnt_links">
                 <p className="user_info_label" onClick={deleteUser}>
                   <Link to="/settings/my-account" classname="ft_Weight_500">Delete Account</Link>
                 </p>
