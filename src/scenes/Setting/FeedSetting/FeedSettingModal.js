f23e48fc76e35927635a928d59ce2bc5b7313607
diksha shivhare <diksha@bitcot.com>
2020-09-30 16:16:59 +0530

    design changes in feed seting modal page

--- src/scenes/Setting/FeedSetting/FeedSettingModal.js
+++ src/scenes/Setting/FeedSetting/FeedSettingModal.js
@@ -61,32 +61,31 @@ const FeedSettingModal = ({setParentLoaded, setEditTag, editTag, existingTags}) 
 
   return (
     <>
-        <div class="hover_bkgr_fricc hover_fullS" id="trigger_addMore_popup" style={{display: "block"}}>
-            <div class="modal-dialog-centered">
-                <div class="popup_cont lps_text_white">
-                    <div class="popupCloseButton">
-                        <Link to="/settings/feed-setting" onClick={e => setEditTag(null)} className="nav-link not_line">
-                            <img src={require("assets/images/icons/icn_close_white.png")} alt="Icon Search"/>
-                        </Link>
-                    </div>
-                    <div class="VspcBtwn">
-                    <div class="lps_hash_tags_wrp hash_tag_block hash_tag_links">
-                        <div class="lps_search">
-                        <div class="inner_form mt_0">
-                            <div class="input_field">
-                            <button class="btn_search" type="button" onClick={e => handleChange(e)}>
-                                <img src={require("assets/images/icons/icn_search_white.svg")} alt="Icon Search"/>
-                            </button>
-                            <input class="input_modify" type="text" value={search.name} onChange={e => setSearch({...search, name: e.target.value})}/>
-                            </div>
-                        </div>
-                        </div>
-                        <div class="hashtag">
-                           {hashTags.map((tag, index) =>
+        <div class="hover_bkgr_fricc full_Hvh" id="trigger_add_popup" style={{display: "block"}}>
+      <div class="modal-dialog-centered">
+        <div class="popup_cont">
+          <div class="popup_body post_poup lps_bg_secondary lps_text_white">
+            <div class="popupCloseButton">
+            <Link to="/settings/feed-setting" onClick={e => setEditTag(null)} className="nav-link not_line">
+              <img src={require("assets/images/icons/icn_close_white.png")}/></Link></div>
+            <div class="lps_search">
+              <div class="inner_form">
+                <div class="input_field">
+                  <button class="btn_search" type="button" onClick={e => handleChange(e)}>
+                    <img src={require("assets/images/icons/icn_search_white.svg")} alt="Search"/>
+                  </button>
+                  <input class="input_modify" type="text" value={search.name} onChange={e => setSearch({...search, name: e.target.value})}/>
+                </div>
+              </div>
+            </div>
+            <div class="hash_tag_block mt_30">
+              <div class="hashtags">
+              {hashTags.map((tag, index) =>
                             <button key={index} className={`theme_btn theme_outline_light ${(!removedTags.includes(tag.name) && existingTags.includes(tag.name)) || selectTags.includes(tag.name) ? "active" : ""}`} onClick={() => toggleHashTag(tag)}>{tag.name}</button>
                            )}
-                        </div>
-                      <div class="hashtag">
+              </div>
+            </div>
+            <div class="hashtag">
                         {
                           count > hashTags.length ?
                           <button onClick={e => loadMore()} class="theme_btn theme_outline_primary text_white min_w_170 theme_btn_rds25 text_uppercase">View more</button>
@@ -94,14 +93,14 @@ const FeedSettingModal = ({setParentLoaded, setEditTag, editTag, existingTags}) 
                         }
                         
                       </div>
-                    </div>
-                    <div class="plans_wrp">
-                        <a onClick={showFavoriteTags} class="theme_btn theme_outline_primary text_white btn_r25 text_uppercase min_w_170 W-50P">add selected</a>
-                    </div>
-                    </div>
-                </div>
+            <div class="post_links post_links_undr">
+              <a onClick={showFavoriteTags} class="theme_btn theme_outline_primary text_white btnr_25 text_uppercase min_w_170">add selected</a>
+              <a href="#" class="lps_link mt_15 btn_block" id="trigger_submit_tag">Can't find what you're looking for</a>
             </div>
+          </div>
         </div>
+      </div>
+    </div>
    
     </>
   );
