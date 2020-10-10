10954f20433862b1444058acc9730f4224b938d4
diksha shivhare <diksha@bitcot.com>
2020-10-10 10:54:22 +0530

    add validation in textarea in user profile

--- src/scenes/Profile/components/EditProfile.js
+++ src/scenes/Profile/components/EditProfile.js
@@ -104,7 +104,7 @@ const EditProfile = ({setIsEdit, user}) => {
                   <div className="mail_about_wrp">
                   </div>
                   <textarea className="input_modify txtarea_modify border_0 brds_0" name="bio" rows="5" onChange={calculateTextareaLength}
-                   value={userForm.bio} />
+                   value={userForm.bio} maxLength="5000" />
 
                   <span className="textRange">{textAreaCount}/50000</span>
                 </div>
