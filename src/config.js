00dd9452ce563b1615e0b44146e00029d9235aca
Abdul Mannan <abdul.bitcot@gmail.com>
2020-10-13 12:16:18 +0530

    changes for base url

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 export const BASE_URL = () => {
   let url;
   if (process.env.REACT_APP_ENV === 'development') {
-    url = "https://lips-api.bitcotapps.com";
+    url = "https://stage-api.lips.social";
   }
   if (process.env.REACT_APP_ENV === 'staging') {
-    url = "https://lips-api.bitcotapps.com";
+    url = "https://stage-api.lips.social";
   }
   if (process.env.REACT_APP_ENV === 'production') {
     console.log("production if");
