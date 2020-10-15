24959e1c4626dae33daad907577b9b232ef798b8
Abdul Mannan <abdul.bitcot@gmail.com>
2020-10-15 11:59:35 +0530

    LIPS-87 Terms and condition page has "Contact Us" button

--- src/scenes/Setting/TermsAndCondition.js
+++ src/scenes/Setting/TermsAndCondition.js
@@ -7,14 +7,6 @@ export default () => (
         <img src={require("assets/images/icons/icn_left_arrow.png")} alt="Icon Arrow" className="lps_header_img" />
         <span className="lp_left_auto text_black ft_Weight_500">Terms &amp; Conditions</span>
       </Link>
-      <div className="top_text_wrp lps_bg_secondary">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipis cing elit, sed do eiusmod tempor incidi. Please reach out if ....
-    </p>
-    <div className="text_center">
-      <Link to="/contact_user" className="theme_btn theme_outline_primary btn_r25 text_uppercase text_white min_w_170">Contact us</Link>
-    </div>
-  </div>
   <div className="lps_container lps_terms_con_wrps bg_grayCCC">
       <TermsAndCondition></TermsAndCondition>
       </div>
