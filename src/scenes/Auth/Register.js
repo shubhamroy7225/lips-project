874d31b968b76ab8dcfc1d05afa4548a985640d6
Abdul Mannan <abdul.bitcot@gmail.com>
2020-10-15 16:43:04 +0530

    changes for registration flow

--- src/scenes/Auth/Register.js
+++ src/scenes/Auth/Register.js
@@ -1,49 +1,60 @@
 import React, { useState, useRef } from "react";
 import { Link, useHistory } from "react-router-dom";
-import SimpleReactValidator from 'simple-react-validator';
+import SimpleReactValidator from "simple-react-validator";
 import * as AuthActions from "redux/actions";
+
+import TermsAndConditionPage from "../OnBoarding/TermsAndCondition";
 export default () => {
+  const [reviewTemsAndCondition, setTermsAndConditionPageActive] = useState(
+    false
+  );
+
   const [user, setUser] = useState({
     user_name: "",
     email: "",
     password: "",
-    confirm_password: ""
+    confirm_password: "",
   });
 
   const history = useHistory();
   //initialize validations
-  const simpleValidator = useRef(new SimpleReactValidator({
-    validators: {
-      sameAs: {  // name the rule
-        message: 'The password and :attribute are not matched.',
-        rule: (val, params, validator) => {
-          return val === params[0]//validator.helpers.testRegex(val,/^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/) && params.indexOf(val) === -1
-        },
+  const simpleValidator = useRef(
+    new SimpleReactValidator({
+      validators: {
+        sameAs: {
+          // name the rule
+          message: "The password and :attribute are not matched.",
+          rule: (val, params, validator) => {
+            return val === params[0]; //validator.helpers.testRegex(val,/^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/) && params.indexOf(val) === -1
+          },
 
-        required: true  // optional
-      }
-    }
-  }));
+          required: true, // optional
+        },
+      },
+    })
+  );
   const [, forceUpdate] = useState();
 
-  const handleSubmit = (e) => {
+  const checkValidation = (e) => {
     e.preventDefault();
-    let userData = {...user, user_name: user.user_name.toLowerCase()}
-
     if (simpleValidator.current.allValid()) {
-      AuthActions.verifyUsername(userData.user_name).then(res => {
-        var updatedUser = { ...userData, session_info: { platform: 'web' } }
-        delete updatedUser.confirm_password;
-        AuthActions.signup({ user: updatedUser }).then(res => {
-          history.push("/terms-and-condition");
-        });
+      AuthActions.verifyUsername(user.user_name.toLowerCase()).then((res) => {
+        setTermsAndConditionPageActive(true);
       });
-    }
-    else {
+    } else {
       simpleValidator.current.showMessages(); //show validation messages
-      forceUpdate(1)
+      forceUpdate(1);
     }
+  };
 
+  const handleSubmit = (e) => {   
+    let userData = { ...user, user_name: user.user_name.toLowerCase() };
+
+    var updatedUser = { ...userData, session_info: { platform: "web" } };
+    delete updatedUser.confirm_password;
+    AuthActions.signup({ user: updatedUser }).then((res) => {
+      history.push("/community-guidelines");
+    });
   };
 
   const [passwordShown, setPasswordShown] = useState(false);
@@ -58,74 +69,171 @@ export default () => {
 
   const handleChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
-    forceUpdate(1)
+    forceUpdate(1);
   };
 
   return (
-    <div id="wrap" className="mt_0">
-      <div className="lps_container mt_0">
-        <div className="lps_flx_vm_jc lps_bg_txt_white lps_bg_secondary on_boarding_wrp on_boardingNChng">
-          <div className="lps_form_wrp">
-            <form onSubmit={handleSubmit}>
-              <article className="text_center lps_logo_center">
-                <a className="logo mb_0" href="#">
-                  <img src={require("assets/images/thumbnails/logo.svg")} alt="Lips Logo" className="header__logo" />
-                  <h5 className="text_uppercase text_white">Register</h5>
-                </a>
-              </article>
-              <div className="lps_fields">
-                <div className="form_group_modify">
-                  <input type="text" className="input_modify" placeholder="Username" name="user_name" value={user.user_name}
-                    onChange={handleChange} onBlur={() => simpleValidator.current.showMessageFor('user_name')} />
-                  {simpleValidator.current.message('user_name', user.user_name, 'required|alpha')}
-                </div>
-                <div className="form_group_modify">
-                  <input type="email" className="input_modify" placeholder="Email" name="email" value={user.email}
-                    onBlur={() => simpleValidator.current.showMessageFor('email')} onChange={handleChange} />
-                  {simpleValidator.current.message('email', user.email, 'required|email')}
-                </div>
-                <div className="form_group_modify lps_pos_rltv">
-                  <input type={passwordShown ? "text" : "password"} className="input_modify" placeholder="Password" name="password" value={user.password}
-                    onChange={handleChange} onBlur={() => simpleValidator.current.showMessageFor('password')} />
-                  <span className="icn_passAbslt">
-                    {passwordShown ? <img onClick={togglePasswordVisibility(false)} src={require("assets/images/icons/icb_eye_hide_black.png")} /> : <img onClick={togglePasswordVisibility(false)} src={require("assets/images/icons/icb_eye_black.png")} />}
-                  </span>
-                  {simpleValidator.current.message('password', user.password, 'required')}
-                </div>
-                <div className="form_group_modify lps_pos_rltv">
-                  <input type={resetPasswordShown ? "text" : "password"} className="input_modify" placeholder="Repeat Password" value={user.confirm_password}
-                    onChange={handleChange} name="confirm_password" onBlur={() => simpleValidator.current.showMessageFor('confirm_password')} />
-                  <span className="icn_passAbslt">
-
-                    {resetPasswordShown ? <img onClick={togglePasswordVisibility(true)} src={require("assets/images/icons/icb_eye_hide_black.png")} /> : <img onClick={togglePasswordVisibility(true)} src={require("assets/images/icons/icb_eye_black.png")} />}
-                  </span>
-                  {simpleValidator.current.message('confirm_password', user.confirm_password, `required|sameAs:${user.password}`)}
-                </div>
-                <div className="mt_25">
-                  <div className="form_group_modify mb_25">
-                    <label className="lps_cont_check">I am 18 years or older
-                      <input type="checkbox" defaultChecked/>
-                      <span className="lps_Checkmark"></span>
-                    </label>
+    <>
+      {reviewTemsAndCondition ? (
+        <TermsAndConditionPage
+          isRegister={true}
+          handleSubmit={handleSubmit}
+          setTermsAndConditionPageActive={setTermsAndConditionPageActive}
+        />
+      ) : (
+        <div id="wrap" className="mt_0">
+          <div className="lps_container mt_0">
+            <div className="lps_flx_vm_jc lps_bg_txt_white lps_bg_secondary on_boarding_wrp on_boardingNChng">
+              <div className="lps_form_wrp">
+                <form onSubmit={checkValidation}>
+                  <article className="text_center lps_logo_center">
+                    <a className="logo mb_0" href="#">
+                      <img
+                        src={require("assets/images/thumbnails/logo.svg")}
+                        alt="Lips Logo"
+                        className="header__logo"
+                      />
+                      <h5 className="text_uppercase text_white">Register</h5>
+                    </a>
+                  </article>
+                  <div className="lps_fields">
+                    <div className="form_group_modify">
+                      <input
+                        type="text"
+                        className="input_modify"
+                        placeholder="Username"
+                        name="user_name"
+                        value={user.user_name}
+                        onChange={handleChange}
+                        onBlur={() =>
+                          simpleValidator.current.showMessageFor("user_name")
+                        }
+                      />
+                      {simpleValidator.current.message(
+                        "user_name",
+                        user.user_name,
+                        "required|alpha"
+                      )}
+                    </div>
+                    <div className="form_group_modify">
+                      <input
+                        type="email"
+                        className="input_modify"
+                        placeholder="Email"
+                        name="email"
+                        value={user.email}
+                        onBlur={() =>
+                          simpleValidator.current.showMessageFor("email")
+                        }
+                        onChange={handleChange}
+                      />
+                      {simpleValidator.current.message(
+                        "email",
+                        user.email,
+                        "required|email"
+                      )}
+                    </div>
+                    <div className="form_group_modify lps_pos_rltv">
+                      <input
+                        type={passwordShown ? "text" : "password"}
+                        className="input_modify"
+                        placeholder="Password"
+                        name="password"
+                        value={user.password}
+                        onChange={handleChange}
+                        onBlur={() =>
+                          simpleValidator.current.showMessageFor("password")
+                        }
+                      />
+                      <span className="icn_passAbslt">
+                        {passwordShown ? (
+                          <img
+                            onClick={togglePasswordVisibility(false)}
+                            src={require("assets/images/icons/icb_eye_hide_black.png")}
+                          />
+                        ) : (
+                          <img
+                            onClick={togglePasswordVisibility(false)}
+                            src={require("assets/images/icons/icb_eye_black.png")}
+                          />
+                        )}
+                      </span>
+                      {simpleValidator.current.message(
+                        "password",
+                        user.password,
+                        "required"
+                      )}
+                    </div>
+                    <div className="form_group_modify lps_pos_rltv">
+                      <input
+                        type={resetPasswordShown ? "text" : "password"}
+                        className="input_modify"
+                        placeholder="Repeat Password"
+                        value={user.confirm_password}
+                        onChange={handleChange}
+                        name="confirm_password"
+                        onBlur={() =>
+                          simpleValidator.current.showMessageFor(
+                            "confirm_password"
+                          )
+                        }
+                      />
+                      <span className="icn_passAbslt">
+                        {resetPasswordShown ? (
+                          <img
+                            onClick={togglePasswordVisibility(true)}
+                            src={require("assets/images/icons/icb_eye_hide_black.png")}
+                          />
+                        ) : (
+                          <img
+                            onClick={togglePasswordVisibility(true)}
+                            src={require("assets/images/icons/icb_eye_black.png")}
+                          />
+                        )}
+                      </span>
+                      {simpleValidator.current.message(
+                        "confirm_password",
+                        user.confirm_password,
+                        `required|sameAs:${user.password}`
+                      )}
+                    </div>
+                    <div className="mt_25">
+                      <div className="form_group_modify mb_25">
+                        <label className="lps_cont_check">
+                          I am 18 years or older
+                          <input type="checkbox" defaultChecked />
+                          <span className="lps_Checkmark"></span>
+                        </label>
+                      </div>
+                      <div className="form_group_modify">
+                        <label className="lps_cont_check">
+                          Keep me signed in
+                          <input type="checkbox" defaultChecked />
+                          <span className="lps_Checkmark"></span>
+                        </label>
+                      </div>
+                    </div>
                   </div>
-                  <div className="form_group_modify">
-                    <label className="lps_cont_check">Keep me signed in
-                      <input type="checkbox" defaultChecked />
-                      <span className="lps_Checkmark"></span>
-                    </label>
+                  <div className="pos_wrp onboarding_btm">
+                    <button
+                      type="submit"
+                      className="theme_btn theme_primary btn_block theme_btn_rds25 text_uppercase W-50P"
+                    >
+                      Review Terms &amp; Conditions
+                    </button>
+                    <p className="btm_links mt_25 text_white">
+                      Have an account?{" "}
+                      <Link to="/login" className="link_underline lps_link">
+                        Sign in
+                      </Link>
+                    </p>
                   </div>
-                </div>
+                </form>
               </div>
-              <div className="pos_wrp onboarding_btm">
-                <button type="submit" className="theme_btn theme_primary btn_block theme_btn_rds25 text_uppercase W-50P">Review
-                        Terms &amp; Conditions</button>
-                <p className="btm_links mt_25 text_white">Have an account? <Link to="/login"
-                  className="link_underline lps_link">Sign in</Link></p>
-              </div>
-            </form>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  )
-}
+      )}
+    </>
+  );
+};
