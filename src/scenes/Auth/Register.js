fdd371f23a8413ca1f381b8c68826e1680c00ad6
pappudas bairagi <pappudas@bitcot.com>
2020-10-16 12:19:45 +0530

    changes for 18 year old checkbox

--- src/scenes/Auth/Register.js
+++ src/scenes/Auth/Register.js
@@ -201,7 +201,7 @@ export default () => {
                       <div className="form_group_modify mb_25">
                         <label className="lps_cont_check">
                           I am 18 years or older
-                          <input type="checkbox" defaultChecked />
+                          <input type="checkbox" checked={true} />
                           <span className="lps_Checkmark"></span>
                         </label>
                       </div>
