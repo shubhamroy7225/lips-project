863cb844bfaec411bc2990962363a33dd6d7cb09
Abdul Mannan <abdul.bitcot@gmail.com>
2020-10-16 19:09:25 +0530

    className fixed error

--- src/scenes/components/AddToHome.js
+++ src/scenes/components/AddToHome.js
@@ -21,17 +21,17 @@ const AddToHome = (props) => {
     }
     return (
         <div className="onTimeModel">
-            <div class="hover_bkgr_fricc hover_alternate_W modal-backdrop" id="trigger_HomeScreen_popup" style={{ display: checkModalOpen()}}>
-                <div class="modal-dialog-centered">
-                    <div class="popup_cont">
-                        <div class="popup_body">
-                            <div class="popupCloseButton" id="popupCloseButton"><a onClick={() => { setHidden(true) }}><img src={require("assets/images/icons/icn_close.png")} /></a></div>
-                            <figure class="lps_fig lps_top_imgs">
+            <div className="hover_bkgr_fricc hover_alternate_W modal-backdrop" id="trigger_HomeScreen_popup" style={{ display: checkModalOpen()}}>
+                <div className="modal-dialog-centered">
+                    <div className="popup_cont">
+                        <div className="popup_body">
+                            <div className="popupCloseButton" id="popupCloseButton"><a onClick={() => { setHidden(true) }}><img src={require("assets/images/icons/icn_close.png")} /></a></div>
+                            <figure className="lps_fig lps_top_imgs">
                                 <img src={require("assets/images/icons/add_to_home_logo.svg")} alt="Image Placeholder" />
                             </figure>
                             <h2>Did you know Lips is an app?</h2>
                             <p>Add Lips to your home screen for a better experience.</p>
-                            <h4>Tap <a href="#" class="inline_WText" onClick={() => { setHidden(true) }}><img src={require("assets/images/icons/icn_upload.png")} alt="Upload" /></a> then 'Add to Home Screen'</h4>
+                            <h4>Tap <a href="#" className="inline_WText" onClick={() => { setHidden(true) }}><img src={require("assets/images/icons/icn_upload.png")} alt="Upload" /></a> then 'Add to Home Screen'</h4>
                         </div>
                     </div>
                 </div>
