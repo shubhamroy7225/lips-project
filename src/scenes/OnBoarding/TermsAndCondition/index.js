37212abf138e9fec9bc726683218754e78155284
pappudas bairagi <pappudas@bitcot.com>
2020-10-17 16:44:12 +0530

    LIPS-114
    [Register] Agree Terms and Conditions - Contact Us content gets displayed

--- src/scenes/OnBoarding/TermsAndCondition/index.js
+++ src/scenes/OnBoarding/TermsAndCondition/index.js
@@ -31,20 +31,6 @@ export default ({
       </div>
 
       <div id="wrap" className="mt_0">
-        <div className="top_text_wrp lps_bg_secondary">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipis cing elit, sed do
-            eiusmod tempor incidi. Please reach out if ....
-          </p>
-          <div className="text_center">
-            <Link
-              to="/contact_user"
-              className="theme_btn theme_outline_primary btn_r25 text_uppercase text_white min_w_170"
-            >
-              Contact us
-            </Link>
-          </div>
-        </div>
         <div className="lps_container lps_terms_con_wrps bg_grayCCC">
           <TermsAndCondition></TermsAndCondition>
           <div className="lps_fixed_btm lps_bg_secondary">
