15d57c92db1023228a96aaba90e840ea2a172462
Abdul Mannan <abdul.bitcot@gmail.com>
2020-10-20 10:48:11 +0530

    https://bitcot.atlassian.net/browse/LIPS-124

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -45,8 +45,7 @@ const Header = ({ notificationCount, notifications, count, ...props }) => {
         )
     } else if (Object.values(PRIVATE_PATH).includes(props.history.location.pathname) ||
         (props.history.location.pathname === routes.ROOT && props.user) ||
-        props.history.location.pathname === routes.MAIN_FEED ||
-        (props.user && !Object.values(SETTINGS_PATH).includes(props.history.location.pathname))) {
+        props.history.location.pathname === routes.MAIN_FEED ) {
         //default when user is not logged in
         let headerClassName = "main_header";
         if (props.history.location.pathname === routes.ROOT) {
