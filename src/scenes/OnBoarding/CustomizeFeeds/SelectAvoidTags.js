671e1221769d60b32fde5124c2c1f0eecb6ae46b
pappudas bairagi <pappudas@bitcot.com>
2020-10-21 12:26:43 +0530

    Merge branch 'user-setting' of bitbucket.org:bitcot/lips-react into staging
    # Conflicts:
    #	src/scenes/OnBoarding/CustomizeFeeds/SelectAvoidTags.js
    #	src/scenes/OnBoarding/CustomizeFeeds/index.js

--- src/scenes/OnBoarding/CustomizeFeeds/SelectAvoidTags.js
+++ src/scenes/OnBoarding/CustomizeFeeds/SelectAvoidTags.js
@@ -20,23 +20,23 @@ export default () => {
       });
    };
    return (
-      <div id="wrap" className="mt_0">
+       <div id="wrap" className="mt_0">
          <div className="lps_container mt_0">
-            <div className="lps_flx_vm_jc lps_bg_secondary lps_text_white on_boarding_wrp">
-               <div className="lps_form_wrp on_boarding_wrp_spwn border_0">
-                  <article className="lps_art lps_art_white">
-                     <h3 className="mb30">Some things are not for everybody - and that’s ok!   </h3>
-                     <h5 className="ft_Weight_400">
-                        Mark the ones you don't want to see and we will do our best to provide tag-based warnings.
-                         </h5>
-                      </article>
-                     <HashTags setSelectTags={setSelectTags} selectTags={selectTags}/>
-                      <div className="pos_wrp onboarding_btm">
-                         <button onClick={addFavoriteTags} className="theme_btn theme_outline_primary text_white btn_block theme_btn_rds25 text_uppercase W-50P desktopVersio">Browse</button>
-                      </div>
-                   </div>
-                </div>
+           <div className="lps_flx_vm_jc lps_bg_secondary lps_text_white on_boarding_wrp">
+             <div className="lps_form_wrp on_boarding_wrp_spwn border_0">
+               <article className="lps_art lps_art_white">
+                 <h3 className="mb30">Some things are not for everybody - and that’s ok!   </h3>
+                 <h5 className="ft_Weight_400">
+                   Mark the ones you don't want to see and we will do our best to provide tag-based warnings.
+                 </h5>
+               </article>
+               <HashTags setSelectTags={setSelectTags} selectTags={selectTags}/>
+               <div className="pos_wrp onboarding_btm">
+                 <button onClick={addFavoriteTags} className="theme_btn theme_outline_primary text_white btn_block theme_btn_rds25 text_uppercase W-50P desktopVersio">Browse</button>
+               </div>
              </div>
-          </div>
-  )
+           </div>
+         </div>
+       </div>
+   )
 }
--- src/scenes/OnBoarding/CustomizeFeeds/index.js
+++ src/scenes/OnBoarding/CustomizeFeeds/index.js
@@ -12,24 +12,24 @@ export default () => {
   };
 
   return (
-    <div id="wrap" className="mt_0">
-      <div className="lps_container mt_0">
-        <div className="lps_flx_vm_jc lps_bg_secondary lps_text_white on_boarding_wrp">
-          <div className="lps_form_wrp on_boarding_wrp_spwn border_0">
-            <article className="lps_art lps_art_white">
-              <h3 className="mb30">Start from scratch or let us fill your feed.</h3>
-              <h5 className="mb30 ft_Weight_400">
-                An auto-populated feed may include content that's not for everyone such as nudity or profanity.
-                    </h5>
-              <h5 className="ft_Weight_400">
-                Can't decide now? You can always adjust your Feed settings later.
-                    </h5>
-                  </article>
-                  <div className="pos_wrp onboarding_btm">
-                    <Link to="/favorite-tags" className="theme_btn theme_outline_primary text_white btn_block theme_btn_rds25 text_uppercase lps_mb10 W-50P desktopVersio">Customize feed</Link>
-                    <button onClick={completeOnBoard} className="theme_btn theme_outline_primary text_white btn_block theme_btn_rds25 text_uppercase W-50P desktopVersio">Auto-Generate</button>
-                  </div>
+      <div id="wrap" className="mt_0">
+        <div className="lps_container mt_0">
+          <div className="lps_flx_vm_jc lps_bg_secondary lps_text_white on_boarding_wrp">
+            <div className="lps_form_wrp on_boarding_wrp_spwn border_0">
+              <article className="lps_art lps_art_white">
+                <h3 className="mb30">Start from scratch or let us fill your feed.</h3>
+                <h5 className="mb30 ft_Weight_400">
+                  An auto-populated feed may include content that's not for everyone such as nudity or profanity.
+                </h5>
+                <h5 className="ft_Weight_400">
+                  Can't decide now? You can always adjust your Feed settings later.
+                </h5>
+              </article>
+              <div className="pos_wrp onboarding_btm">
+                <Link to="/favorite-tags" className="theme_btn theme_outline_primary text_white btn_block theme_btn_rds25 text_uppercase lps_mb10 W-50P desktopVersio">Customize feed</Link>
+                <button onClick={completeOnBoard} className="theme_btn theme_outline_primary text_white btn_block theme_btn_rds25 text_uppercase W-50P desktopVersio">Auto-Generate</button>
               </div>
+            </div>
           </div>
         </div>
       </div>
