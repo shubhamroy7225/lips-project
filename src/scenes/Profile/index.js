4f92b206db9ede7cae9c07043e85fb6d7c9ecabc
Abdul Mannan <abdul.bitcot@gmail.com>
2020-10-23 15:25:38 +0530

    3. The "Image" tab on the "My profile" slightly overlaps on the first image. Need to add some extra space so that the tab and the line doesn't overlap with the images on the first row.

--- src/scenes/Profile/index.js
+++ src/scenes/Profile/index.js
@@ -237,7 +237,7 @@ const Profile = (props) => {
                         <div id="tab-1" class="tab-content_cst current pt_0">
                             {
                                 gridlayoutMode ?
-                                    <div class="lps_product_grid destkVersion">
+                                    <div class="lps_product_grid destkVersion mt_5">
                                         {gridFeedContent}
                                     </div>
                                     :
