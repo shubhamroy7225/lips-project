440c34f905b3e7cc0f27010ca86f12355bc1aa9a
Abdul Mannan <abdul.bitcot@gmail.com>
2020-10-28 17:57:20 +0530

    replace owl caroual with owl carousel 2

--- src/scenes/OnBoarding/InitialLanding/index.js
+++ src/scenes/OnBoarding/InitialLanding/index.js
@@ -1,9 +1,17 @@
-import React from "react";
-
+import React, {useRef} from "react";
 import { Link } from "react-router-dom";
 
+import OwlCarousel from 'react-owl-carousel2';
 
 export default () => {
+  const ref = useRef();
+  const options = {
+    items: 1,
+    animateOut: "fadeOut",
+    loop: true,
+    rewind: true,
+    autoplay: true
+};
   return (
       <>
       <div id="wrap" className="mt_0">
@@ -16,39 +24,24 @@ export default () => {
                     <img src={require("assets/images/thumbnails/logo.svg")} alt="Lips Logo" className="header__logo" />
                   </Link>
                 </article>
-                <div className="product_grid_block saved_product pd_y brand_product_wrp px_0">
-                  <div className="owl-carousel owl-theme lps_slider product_slider1 pd_t10 brand_pro_slider" id="product_slider2">
+                <div className="product_grid_block saved_product pd_y brand_product_wrp px_0 landingCarouse">
+                <OwlCarousel ref={ref} options={options} >
                     <div className="item">
                       <div className="product_item_wrp">
-                        <div className="product_item">
-                          <figure className="lps_fig">
-                            {/* <img src={require("assets/images/thumbnails/thumbnail1.png")} alt="thumbnail1" /> */}
-                          </figure>
-                        </div>
                         <h5>A creative space for women-identified &amp; LGBTQIA+ creators</h5>
                       </div>
                     </div>
                     <div className="item">
                       <div className="product_item_wrp">
-                        <div className="product_item">
-                          <figure className="lps_fig">
-                            {/* <img src={require("assets/images/thumbnails/thumbnail1.png")} alt="thumbnail1" /> */}
-                          </figure>
-                        </div>
                         <h5>Share your work and explore content by the creators you love</h5>
                       </div>
                     </div>
                     <div className="item">
                       <div className="product_item_wrp">
-                        <div className="product_item">
-                          <figure className="lps_fig">
-                            {/* <img src={require("assets/images/thumbnails/thumbnail1.png")} alt="thumbnail1" /> */}
-                          </figure>
-                        </div>
                         <h5>Express yourself without biased censorship or online harassment</h5>
                       </div>
                     </div>
-                  </div>
+                  </OwlCarousel>                  
                 </div>
                 <div className="pos_wrp">
                   <Link to="/login" className="theme_btn theme_primary btn_block theme_btn_rds25 text_uppercase lps_mb10 W-50P desktopVersio">
