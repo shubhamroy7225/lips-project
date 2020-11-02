9823b1c4fb751a88a5d4b8268085b618f37dc5fe
jsahil <sahil@bitcot.com>
2020-11-02 11:34:18 +0530

    fixing image feed when there is no image

--- src/scenes/Feed/components/ImageItem.js
+++ src/scenes/Feed/components/ImageItem.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
+import placeholder from 'assets/images/thumbnails/thumb_placeholder.png';
 
 const ImageItem = ({ feed, selectionHandler, isReposted }) => {
     const { attachments } = isReposted ? feed.parent : feed;
-    const { photo_urls } = attachments[0]
+    const { photo_urls } = attachments.length > 0 ? attachments[0] : { photo_urls: { medium: placeholder } }; //else part is to handle the crash in case there is no image for feed type = "image"
 
     return (
         <div class="product_card" onClick={selectionHandler}>
