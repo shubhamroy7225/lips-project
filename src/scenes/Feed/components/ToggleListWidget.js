66998a357f0a2eaf58c1083a69b8a9eabaf0df73
jsahil <sahil@bitcot.com>
2020-11-06 14:39:45 +0530

    toggle icon interchanged - list/columnview

--- src/scenes/Feed/components/ToggleListWidget.js
+++ src/scenes/Feed/components/ToggleListWidget.js
@@ -10,16 +10,16 @@ const ToggleListWidget = ({ gridlayoutMode, setGridLayoutMode }) => {
         setGridLayoutMode(!gridlayoutMode)
     }
 
-    let className = "horizantal_coll1 icn_hover_chng"
+    let className = "horizantal_coll1 icn_hover_chng active"
     if (!gridlayoutMode) {
-        className = "horizantal_coll1 icn_hover_chng active"
+        className = "horizantal_coll1 icn_hover_chng"
     }
 
     return (
         <div class="footer-menu-list1 footIndex">
             <a onClick={toggleState} class={className} id="trigger_popup_fricc1">
-                <img src={require("assets/images/icons/grid.svg")} class="inner_image icn_dfltD" alt="Repeat Icon" />
                 <img src={require("assets/images/icons/list.svg")} class="inner_image icn_hvrA" alt="Repeat Icon" />
+                <img src={require("assets/images/icons/grid.svg")} class="inner_image icn_dfltD" alt="Repeat Icon" />
             </a>
         </div>
     )
