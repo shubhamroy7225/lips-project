dcab336637793c882688cbbe063c36686181ac46
Abdul Mannan <abdul.bitcot@gmail.com>
2020-11-10 18:14:32 +0530

    LIPS-229  [Client Need] Change the column view icon as attached -PFA

--- src/scenes/Feed/components/ToggleListWidget.js
+++ src/scenes/Feed/components/ToggleListWidget.js
@@ -18,8 +18,8 @@ const ToggleListWidget = ({ gridlayoutMode, setGridLayoutMode }) => {
     return (
         <div class="footer-menu-list1 footIndex">
             <a onClick={toggleState} class={className} id="trigger_popup_fricc1">
-                <img src={require("assets/images/icons/list.svg")} class="inner_image icn_hvrA" alt="Repeat Icon" />
-                <img src={require("assets/images/icons/grid.png")} class="inner_image icn_dfltD boxIcon" alt="Repeat Icon" />
+                <img src={require("assets/images/icons/list.png")} class="inner_image icn_hvrA boxIcon" alt="Repeat Icon" />
+                <img src={require("assets/images/icons/grid.svg")} class="inner_image icn_dfltD boxIcon" alt="Repeat Icon" />
             </a>
         </div>
     )
