16bc222a6d7cf55e2ece8ff80bceb54b82d42504
diksha shivhare <diksha@bitcot.com>
2020-12-04 15:46:23 +0530

    LIPS-298 Whenever user received any notification from admin then lips logo should be shown instead user placeholder image in notification

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -244,7 +244,7 @@ const NotificationSliderComponent = ({ modalShown, modalToggle }) => {
                             <div className="lps_media">
                                 <figure className="lps_fig lps_fig_circle notfication_lips_logo">
                                     {notification.content === "You've been approved. You can post now" ? 
-                                        <img src={require("assets/images/thumbnails/new_logo.svg")} alt="Lips Logo" className="header__logo notifcation_logo" /> :
+                                        <img src={require("assets/images/thumbnails/lips_Small.png")} alt="Lips Logo" className="header__logo notifcation_logo" /> :
                                         <img src={notification.user && notification.user.photo_urls.medium ? notification.user.photo_urls.medium : require("assets/images/icons/icn_profile.svg")} alt="User" />
                                     }
                                 </figure>
