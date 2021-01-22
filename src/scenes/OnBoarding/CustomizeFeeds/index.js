1867e93e8f5fb782dc0f9f4b553fbb260fd677ae
diksha shivhare <diksha@bitcot.com>
2021-01-22 17:42:04 +0530

    fixed button in just browse page in IOS device

--- src/scenes/OnBoarding/CustomizeFeeds/index.js
+++ src/scenes/OnBoarding/CustomizeFeeds/index.js
@@ -28,7 +28,7 @@ export default () => {
         </div>
         <div className="lps_container mt_0">
           <div className="lps_flx_vm_jc lps_bg_secondary lps_text_white on_boarding_wrp">
-            <div className="lps_form_wrp on_boarding_wrp_spwn border_0 commonWidth on_boarding_top">
+            <div className="lps_form_wrp on_boarding_wrp_spwn on_boarding_wrp_around border_0 commonWidth on_boarding_top">
               <article className="lps_art lps_art_white">
                 <h3 className="mb30">start from scratch or let us fill your feed</h3>
                 <h5 className="mb30 ft_Weight_400">
